06762cfcca55ec5ee5ddcb0323e746ea5371bd46
amarhbaneen <amaralzana@gamil.com>
2021-11-09 18:59:15 +0200

    made the code can run by cmd line used the bitewise to remove the yellow line added readme file

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 import glob
-
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-
 import os
+import sys
 
-import cv2
-import matplotlib.pyplot as plt
+import cv2 as cv
 import numpy as np
 
-n = cv2.imread('text_regions/BRN3C2AF4AEB56C_0000000015.jpg')
-hvs_img = cv2.cvtColor(n,cv2.COLOR_BGR2HSV)
-lower_yellow = np.array([21,39,64])
-upper_yellow = np.array([40,255,255])
+
+def removelines(img):
+    img1 = cv.imread(img)  # reading the image from path that received from the function arguments
+    hvs_img = cv.cvtColor(img1, cv.COLOR_BGR2HSV)  # changing the color of the image from BGR model to HSV model
+    lower_yellow = np.array([21, 39, 114])  # getting the lower level of yellow color in HSV model
+    upper_yellow = np.array([40, 255, 255])  # getting the highest level of yellow color in HSV model
+    mask = cv.inRange(hvs_img, lower_yellow, upper_yellow)  # creating the mask of the yellow lines from the image
+    mask_inv = cv.bitwise_not(mask)  # created inverted mask from the mask that created
+    img1_bg = cv.bitwise_and(img1, img1, mask=mask_inv)  # using the mask that created and bitwise  "and operator " to
+    # create the background
+    img2_fg = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)  # because the mask is GRAYSCALE that mean its 1 channel
+    # and the cv.add need to be same number of channels , using cvtColor switch from GRAYSCALE to BGR and the mask
+    # became 3 channels
+    dst = cv.add(img1_bg, img2_fg)  # merging the two images ( mask and HSV image ) to get the final result
+    return dst
 
 
-mask = cv2.inRange(hvs_img,lower_yellow,upper_yellow)
-new_mask = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
-new_image = cv2.bitwise_and(new_mask,hvs_img)
-plt.imshow(new_image,cmap='gray')
-plt.show()
+def loopRun(inputs, output):
+    path = os.path.basename(inputs)  # getting the basename from the path that received
+    # e.g. C:\Users\amara\Desktop\new => new
+
+    output = os.path.normpath(output)  # changing the format of path that received
+    # from String format to path
+    # e.g. C:\Users\amara\Desktop\new => C:\\Users\\amara\\Desktop\\new
+    for img in glob.glob(path + "/*.jpg"):  # loop to get every image in the dir
+        filename = os.path.basename(img)  # getting the file name (image name )
+        fixed_image = removelines(img)  # receiving the image after removing the yellow lines
+        cv.imwrite(output + '\\' + filename, fixed_image)  # writing (saving) the image to the path
 
 
-
-# for img in glob.glob("text_regions/*.jpg"):
-#     n= cv2.imread(img,1)
-#
-#     filename = os.path.basename(img)
-#     path = 'D:\\'
-#     cv2.imwrite(path + filename , n)
-#
+if __name__ == "__main__":
+    inputPath = sys.argv[1] # getting the input path from the command line
+    outputPath = sys.argv[2] # getting the output path from the command line
+    if os.path.isdir(inputPath): # checking if the input string is an exist path
+        if os.path.isdir(outputPath):  # checking if the output string is an exist path
+            loopRun(inputPath, outputPath)
+        else:
+            print("output dir is not valid ")
+    else:
+        print("input dir is not valid ")
---
+++ readme.md
@@ -0,0 +1,25 @@
+#LINES REMOVER 
+#### all rights reserved to Amar Alsana ID 208971549 ®
+###the main idea from the code is to remove the yellow line from the photo
+#### the program read all the image that located inside the input path 
+#### and remove the yellow line from the photo and save the new image to output path 
+### ******requirements before running the code******
+##### OpenCv install by `pip install opencv-contrib-python`
+##### Python 3.9 and older [https://www.python.org/downloads/](https://www.python.org/downloads/)
+
+## How to run
+   ##### run by using the CMD where main.py located 
+    python [Code Name e.g. main.py]  [Input Path] [ Output Path] 
+   ##### 1) run by absolute path 
+    python main.py C:\Users\amara\PycharmProjects\pythonProject3\text_regions C:\Users\amara\Desktop\new
+   ##### 2) run by local file that located in same path of main.py 
+    python main.py text_regions test
+
+##Example
+### Before running the program 
+#### all rights reserved to  Dr. Irina Rabaev 
+![](C:\Users\amara\PycharmProjects\pythonProject3\text_regions\BRN3C2AF4AEB56C_0000000013.jpg)
+
+### after running the program 
+#### all rights reserved to  Dr. Irina Rabaev 
+![](C:\Users\amara\Desktop\new\BRN3C2AF4AEB56C_0000000013.jpg)
